0371ab40e02e3de3b80a7572606aa8819e541779
Nishant Chatterjee <nishantc1527@gmail.com>
2020-10-31 11:05:40 -0700

    changed edit distance to bottom up

--- src/EditDistance.java
+++ src/EditDistance.java
@@ -4,33 +4,30 @@ public class EditDistance {
     System.out.println(new EditDistance().minDistance("horse", "ros"));
   }
 
-  public int minDistance(String word1, String word2) {
-    return minDistance(word1, word2, new int[word1.length()][word2.length()], 0, 0);
-  }
+  public int minDistance(String word1String, String word2String) {
+    char[] word1 = word1String.toCharArray();
+    char[] word2 = word2String.toCharArray();
+    int[][] dp = new int[word1.length + 1][word2.length + 1];
 
-  private int minDistance(String word1, String word2, int[][] memo, int curr1, int curr2) {
-    if(curr1 >= word1.length() && curr2 >= word2.length()) {
-      return 0;
+    for (int i = 1; i < dp.length; i++) {
+      dp[i][0] = i;
     }
 
-    if(curr1 >= word1.length()) {
-      return word2.length() - curr2;
+    for (int i = 1; i < dp[0].length; i++) {
+      dp[0][i] = i;
     }
 
-    if(curr2 >= word2.length()) {
-      return word1.length() - curr1;
+    for (int i = 1; i < dp.length; i++) {
+      for (int j = 1; j < dp[i].length; j++) {
+        if (word1[i - 1] == word2[j - 1]) {
+          dp[i][j] = dp[i - 1][j - 1];
+        } else {
+          dp[i][j] = 1 + Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]);
+        }
+      }
     }
 
-    if (memo[curr1][curr2] != 0) {
-      return memo[curr1][curr2];
-    }
-
-    if(word1.charAt(curr1) == word2.charAt(curr2)) {
-      return memo[curr1][curr2] = minDistance(word1, word2, memo, curr1 + 1, curr2 + 1);
-    }
-
-    return memo[curr1][curr2] = 1 + Math.min(minDistance(word1, word2, memo, curr1 + 1, curr2),
-            Math.min(minDistance(word1, word2, memo, curr1, curr2 + 1), minDistance(word1, word2, memo, curr1 + 1, curr2 + 1)));
+    return dp[dp.length - 1][dp[0].length - 1];
   }
 
 }
