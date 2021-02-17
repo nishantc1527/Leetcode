2627b8d96dda22c76b8a67039a8628f7ba373575
Nishant Chatterjee <nishantc1527@gmail.com>
2021-02-17 15:19:31 -0800

    Optimized Swimming In Rising Water Problem

--- src/SwimInRisingWater.java
+++ src/SwimInRisingWater.java
@@ -1,42 +1,47 @@
-import utils.Pair;
-
 import java.util.*;
 
 public class SwimInRisingWater {
 
   public static void main(String[] args) {
-    System.out.println(new SwimInRisingWater().swimInWater(new int[][]{{0, 2},{1, 3}}));
+    System.out.println(new SwimInRisingWater().swimInWater(new int[][]{{0, 2}, {1, 3}}));
+  }
+
+  class Trio {
+
+    private int a, b, c;
+
+    public Trio(int a, int b, int c) {
+      this.a = a;
+      this.b = b;
+      this.c = c;
+    }
+
   }
 
   public int swimInWater(int[][] grid) {
-    PriorityQueue<Pair<Integer, Integer>> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(a -> grid[a.getKey()][a.getValue()]));
-    priorityQueue.offer(new Pair<>(0, 0));
-    boolean[][] visited = new boolean[grid.length][grid.length];
-    int max = -1;
+    int n = grid.length, res = 0;
+    PriorityQueue<Trio> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.a, b.a));
+    pq.offer(new Trio(grid[0][0], 0, 0));
 
-    while(!priorityQueue.isEmpty()) {
-      Pair<Integer, Integer> top = priorityQueue.poll();
-      max = Math.max(max, grid[top.getKey()][top.getValue()]);
-      visited[top.getKey()][top.getValue()] = true;
+    boolean[][] seen = new boolean[n][n];
+    seen[0][0] = true;
 
-      if(top.getKey() == grid.length - 1 && top.getValue() == grid.length - 1) {
-        return max;
+    int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    while (!pq.isEmpty()) {
+      Trio top = pq.poll();
+      res = Math.max(res, top.a);
+
+      if (top.b == n - 1 && top.c == n - 1) {
+        return res;
       }
 
-      if (top.getKey() != 0 && !visited[top.getKey() - 1][top.getValue()]) {
-        priorityQueue.offer(new Pair<>(top.getKey() - 1, top.getValue()));
-      }
-
-      if (top.getValue() != 0 && !visited[top.getKey()][top.getValue() - 1]) {
-        priorityQueue.offer(new Pair<>(top.getKey(), top.getValue() - 1));
-      }
-
-      if (top.getKey() != grid.length - 1 && !visited[top.getKey() + 1][top.getValue()]) {
-        priorityQueue.offer(new Pair<>(top.getKey() + 1, top.getValue()));
-      }
-
-      if (top.getValue() != grid.length - 1 && !visited[top.getKey()][top.getValue() + 1]) {
-        priorityQueue.offer(new Pair<>(top.getKey(), top.getValue() + 1));
+      for (int[] d : directions) {
+        int i = top.b + d[0], j = top.c + d[1];
+        if (i >= 0 && i < n && j >= 0 && j < n && !seen[i][j]) {
+          seen[i][j] = true;
+          pq.offer(new Trio(grid[i][j], i, j));
+        }
       }
     }
 
