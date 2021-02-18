cbd0aeba324f187b354c43c20b1cff94f64af8e4
Nishant Chatterjee <nishantc1527@gmail.com>
2021-02-17 17:10:30 -0800

    Used Binary Search Solution In Swimming In Rising Water Problem

--- src/SwimInRisingWater.java
+++ src/SwimInRisingWater.java
@@ -1,51 +1,49 @@
-import java.util.*;
-
 public class SwimInRisingWater {
 
   public static void main(String[] args) {
     System.out.println(new SwimInRisingWater().swimInWater(new int[][]{{0, 2}, {1, 3}}));
   }
 
-  class Trio {
-
-    private int a, b, c;
-
-    public Trio(int a, int b, int c) {
-      this.a = a;
-      this.b = b;
-      this.c = c;
-    }
-
-  }
+  private int[][] dirs = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
 
   public int swimInWater(int[][] grid) {
-    int n = grid.length, res = 0;
-    PriorityQueue<Trio> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.a, b.a));
-    pq.offer(new Trio(grid[0][0], 0, 0));
+    int N = grid.length;
+    int lo = 0;
+    int hi = N * N - 1;
 
-    boolean[][] seen = new boolean[n][n];
-    seen[0][0] = true;
-
-    int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
-    while (!pq.isEmpty()) {
-      Trio top = pq.poll();
-      res = Math.max(res, top.a);
-
-      if (top.b == n - 1 && top.c == n - 1) {
-        return res;
-      }
-
-      for (int[] d : directions) {
-        int i = top.b + d[0], j = top.c + d[1];
-        if (i >= 0 && i < n && j >= 0 && j < n && !seen[i][j]) {
-          seen[i][j] = true;
-          pq.offer(new Trio(grid[i][j], i, j));
-        }
+    while (lo < hi) {
+      int middle = (lo + hi) / 2;
+      boolean[][] visited = new boolean[N][N];
+      
+      if (hasPath(grid, 0, 0, middle, visited)) {
+        hi = middle;
+      } else {
+        lo = middle + 1;
       }
     }
 
-    return -1;
+    return lo;
+  }
+
+  private boolean hasPath(int[][] grid, int i, int j, int time, boolean[][] visited) {
+    int N = grid.length;
+    
+    if (i == N - 1 && j == N - 1) {
+      return true;
+    }
+    
+    visited[i][j] = true;
+
+    for (int[] dir : dirs) {
+      int newI = i + dir[0];
+      int newJ = j + dir[1];
+
+      if (newI >= 0 && newJ >= 0 && newI < N && newJ < N && !visited[newI][newJ] && grid[i][j] <= time && grid[newI][newJ] <= time && hasPath(grid, newI, newJ, time, visited)) {
+        return true;
+      }
+    }
+
+    return false;
   }
 
 }
