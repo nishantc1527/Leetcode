cba61239bc5ae6635392760909bb085a3a53d7f5
Nishant Chatterjee <nishantc1527@gmail.com>
2021-05-26 19:18:47 -0700

    Correct Class Name

--- src/LongestSubstringWithoutRepeatingCharacters.java
+++ src/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,10 +1,10 @@
 import java.util.Arrays;
 import java.util.HashMap;
 
-public class LongestPalindromicSubstring {
+public class LongestSubstringWithoutRepeatingCharacters {
 
   public static void main(String[] args) {
-    System.out.println(new LongestPalindromicSubstring().lengthOfLongestSubstring("dvdf"));
+    System.out.println(new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring("dvdf"));
   }
 
   public int lengthOfLongestSubstring(String s) {
